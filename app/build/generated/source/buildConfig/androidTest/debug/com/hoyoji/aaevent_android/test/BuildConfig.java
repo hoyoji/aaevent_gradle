3650d221071fa997b61e87edd3047821d8c1e4ab
benyiu <szepan.yiu@gmail.com>
2015-03-27 20:53:45 +0800

    init commit

--- app/build/generated/source/buildConfig/androidTest/debug/com/hoyoji/aaevent_android/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/hoyoji/aaevent_android/test/BuildConfig.java
@@ -1,13 +1,13 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.hoyoji.aaevent_android;
+package com.hoyoji.aaevent_android.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.hoyoji.aaevent_android";
+  public static final String APPLICATION_ID = "com.hoyoji.aaevent_android.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2411;
+  public static final int VERSION_CODE = -1;
   public static final String VERSION_NAME = "";
 }
